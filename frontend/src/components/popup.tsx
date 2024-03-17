b59e7152aa3ac191c5e84250ffc491a26099c3ff
Chris H <officialchrisho@gmail.com>
2024-03-16 17:28:56 -0700

    Working on a popup spinner redirect after onclick

--- frontend/src/components/popup.tsx
+++ frontend/src/components/popup.tsx
@@ -1,4 +1,16 @@
+import React from 'react';
+import '../spinner.css';
 
+const Popup = () => {
+  return (
+    <div className='absolute z-30'>
+      <h1 className="h1">You will be redirected to our ordering website: Touchnet</h1>
+      <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    </div>
+  );
+};
+
+export default Popup;
 
 
 
--- frontend/src/pages/customer.tsx
+++ frontend/src/pages/customer.tsx
@@ -1,21 +1,27 @@
 import 'tailwindcss/tailwind.css';
 
+import { useState } from 'react';
+
 // import Navbar from '../components/navbar.tsx';
 import Splash from '../components/splash.tsx';
 import Products from '../components/products.tsx';
-// import Popup from '../components/popup.tsx';
+import Popup from '../components/popup.tsx';
 // import Footer from '../components/footer.tsx';
 
 const CustomerPage: React.FC = () => {
+  const [popupState, setPopupState] = useState(false);
+
   return (
     //this is the main page for laughlab.org
-    <div>
+    <div onClick={handleClick}>
       {/* <Navbar/> */}
-      <Splash/>
+      <Splash />
       <p>Welcome to the Main page!</p>
 
       <h1 className='h1'>Products</h1>
-      {/* <Popup/> */}
+      onClick={handleClick}
+      {popupState && <Popup />}
+
       <Products />
       {/* Footer */}
     </div>
@@ -23,3 +29,7 @@ const CustomerPage: React.FC = () => {
 };
 
 export default CustomerPage;
+
+function handleClick() {
+  setPopupState(true);
+}
